100a253366841fc0491611cf803a10782898b37d
Jookie262 <barrientos.134038@kalibo.sti.edu.ph>
2022-11-06 09:07:49 +0800

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AklasWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
