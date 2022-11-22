3c1bba45d40e2bb6bd6255b5168ce8d00db37cb5
Jookie262 <barrientos.134038@kalibo.sti.edu.ph>
2022-11-22 22:01:58 +0700

    Create Admin Login Page

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -13,5 +13,10 @@ namespace AklasWeb.Controllers
         {
             return View();
         }
+
+        public IActionResult Admin()
+        {
+            return View();
+        }
     }
 }
