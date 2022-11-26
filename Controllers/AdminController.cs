c74a70e75148275eac05d4a98441eedafdd2c5ce
Jookie262 <barrientos.134038@kalibo.sti.edu.ph>
2022-11-26 18:42:39 +0700

    Create Admin Dashboard Page

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -2,7 +2,7 @@
 
 namespace AklasWeb.Controllers
 {
-    public class DashboardController : Controller
+    public class AdminController : Controller
     {
         public IActionResult Index()
         {
